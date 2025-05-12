e014cbad1e2dc8cfa42bca599d51ecc6815f8acf
Alisson Lima <alissongdev@gmail.com>
2025-05-12 20:18:26 -0300

    refatora configuração do aplicativo para incluir interceptores HTTP e ajustes no tema do PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,48 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { routes } from './app.routes';
+import { definePreset } from '@primeng/themes';
 import Aura from '@primeng/themes/aura';
+import { API_CONFIG, apiConfig } from './shared/config/api.config';
+import { authInterceptor } from './shared/interceptors/auth.interceptor';
+import { provideAppInitialization } from './app.initializer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(
+      withInterceptorsFromDi(),
+      withInterceptors([authInterceptor])
+    ),
+    { provide: API_CONFIG, useValue: apiConfig },
+    provideAppInitialization(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: definePreset(Aura, {
+          semantic: {
+            primary: {
+              50: '{blue.50}',
+              100: '{blue.100}',
+              200: '{blue.200}',
+              300: '{blue.300}',
+              400: '{blue.400}',
+              500: '{blue.500}',
+              600: '{blue.600}',
+              700: '{blue.700}',
+              800: '{blue.800}',
+              900: '{blue.900}',
+              950: '{blue.950}',
+            },
+          },
+        }),
         options: {
           darkModeSelector: false || 'none',
         },
